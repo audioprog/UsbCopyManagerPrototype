fa5e7d083d44475dbb8c33b8ce60958a654ee434
Leonhard Suckau <web.n.work.ls@mailbox.org>
2019-03-14 20:41:25 +0100

    Windows Usb Watcher eingefügt

--- AvaloniaApplication1/ViewModels/UsbDriveEvents.cs
+++ AvaloniaApplication1/ViewModels/UsbDriveEvents.cs
@@ -6,11 +6,13 @@ namespace AvaloniaApplication1.ViewModels
 {
     public class UsbDriveEvents
     {
+        private Process process;
+
         public UsbDriveEvents()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
             {
-                var process = new Process();
+                process = new Process();
                 process.StartInfo.FileName = "udevadm";
                 process.StartInfo.Arguments = "monitor --udev --property";
                 process.StartInfo.UseShellExecute = false;
@@ -22,6 +24,23 @@ namespace AvaloniaApplication1.ViewModels
                     {
 
                     }//DEVNAME=
+                    Console.WriteLine(data.Data);
+                };
+                process.StartInfo.RedirectStandardError = true;
+                process.ErrorDataReceived += (sender, data) => {
+                    Console.WriteLine(data.Data);
+                };
+                process.Start();
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                process = new Process();
+                process.StartInfo.FileName = "dotnet";
+                process.StartInfo.Arguments = @"..\..\..\..\WindowsUsbWatcher\bin\Debug\netcoreapp2.1\WindowsUsbWatcher.dll";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.OutputDataReceived += (sender, data) => {
                     Console.WriteLine(data.Data);
                 };
                 process.StartInfo.RedirectStandardError = true;
---
+++ WindowsUsbWatcher/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Management;
+
+namespace WindowsUsbWatcher
+{
+    class Program
+
+    {
+        public enum EventType
+        {
+            Inserted = 2,
+            Removed = 3
+        }
+
+        static void Main(string[] args)
+
+        {
+            ManagementEventWatcher watcher = new ManagementEventWatcher();
+            WqlEventQuery query = new WqlEventQuery("SELECT * FROM Win32_VolumeChangeEvent WHERE EventType = 2 or EventType = 3");
+
+            watcher.EventArrived += (s, e) =>
+            {
+                string driveName = e.NewEvent.Properties["DriveName"].Value.ToString();
+                EventType eventType = (EventType)(Convert.ToInt16(e.NewEvent.Properties["EventType"].Value));
+
+                string eventName = Enum.GetName(typeof(EventType), eventType);
+
+                Console.WriteLine("{0} {1}", eventName, driveName);
+            };
+
+            watcher.Query = query;
+            watcher.Start();
+
+            for (; ; );
+
+        }
+    }
+}
