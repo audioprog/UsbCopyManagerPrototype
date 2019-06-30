7503f6b5475ba5c805716827e1601a95640cd551
Leonhard Suckau <web.n.work.ls@mailbox.org>
2019-06-30 17:41:41 +0200

    Auf DataGrid umgestellt

--- AvaloniaApplication1/ViewModels/ViewItemConverter.cs
+++ AvaloniaApplication1/ViewModels/ViewItemConverter.cs
@@ -11,6 +11,29 @@ using System.Text;
 
 namespace AvaloniaApplication1.ViewModels
 {
+
+    public class DateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Retrieve the format string and use it to format the value.
+            string formatString = parameter as string;
+            if (!string.IsNullOrEmpty(formatString))
+            {
+                return string.Format(culture, formatString, value);
+            }
+
+            return value.ToString();
+        }
+
+        // No need to implement converting back on a one-way binding
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
+
     class ViewItemButton : ReactiveObject
     {
         public int DbId { get; set; }
